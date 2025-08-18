56ae197aab06f72eab858ec4874988af02ff96a8
Stas <122303615+Stasss1985@users.noreply.github.com>
2025-08-18 10:37:41 +0300

    login_page.open_page() change on https://www.google.com/

--- tests/test_add_product_no_contract.py
+++ tests/test_add_product_no_contract.py
@@ -3,22 +3,23 @@ import time
 from selenium.webdriver.common.by import By
 
 
-def test_add_gold_no_contract(open_crm_as_admin, lead_page, product_page):
-    # Смена офиса
-    lead_page.change_office_1_maya()
-    lead_page.take_screenshot()
+def test_add_gold_no_contract(login_page):
+    login_page.open_page()
+    #Смена офиса
+    #lead_page.change_office_1_maya()
+    #lead_page.take_screenshot()
     # Переход к созданию лида
-    lead_page.navigate_to_lead_create()
-    lead_page.take_screenshot()
-    time.sleep(4)
+    #lead_page.navigate_to_lead_create()
+    #lead_page.take_screenshot()
+   # time.sleep(4)
     # Добавление продукта
-    product_page.scroll_by(500)
-    time.sleep(2)
-    product_page.add_product_gold()
-    product_page.take_screenshot()
+    #product_page.scroll_by(500)
+    #time.sleep(2)
+    #product_page.add_product_gold()
+   # product_page.take_screenshot()
     # Сравнение названия добавленного товара
-    product_page.compare_name_product()
-    product_page.take_screenshot()
+    #product_page.compare_name_product()
+    #product_page.take_screenshot()
 
 
 def test_add_gold_qr_no_contract(open_crm_as_admin, lead_page, product_page):
