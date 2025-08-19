c52fbf329039b5509e8ea520774b591333761858
Stas <122303615+Stasss1985@users.noreply.github.com>
2025-08-19 10:27:38 +0300

    add marks to tests

--- tests/test_add_product_no_contract.py
+++ tests/test_add_product_no_contract.py
@@ -1,27 +1,31 @@
 import allure
 import time
+import pytest
 from selenium.webdriver.common.by import By
 
 
+@pytest.mark.smoke
 def test_add_gold_no_contract(login_page):
     login_page.open_page()
-    #Смена офиса
-    #lead_page.change_office_1_maya()
-    #lead_page.take_screenshot()
+    # Смена офиса
+    # lead_page.change_office_1_maya()
+    # lead_page.take_screenshot()
     # Переход к созданию лида
-    #lead_page.navigate_to_lead_create()
-    #lead_page.take_screenshot()
-   # time.sleep(4)
-    # Добавление продукта
-    #product_page.scroll_by(500)
-    #time.sleep(2)
-    #product_page.add_product_gold()
-   # product_page.take_screenshot()
-    # Сравнение названия добавленного товара
-    #product_page.compare_name_product()
-    #product_page.take_screenshot()
+    # lead_page.navigate_to_lead_create()
+    # lead_page.take_screenshot()
 
 
+# time.sleep(4)
+# Добавление продукта
+# product_page.scroll_by(500)
+# time.sleep(2)
+# product_page.add_product_gold()
+# product_page.take_screenshot()
+# Сравнение названия добавленного товара
+# product_page.compare_name_product()
+# product_page.take_screenshot()
+
+@pytest.mark.smoke
 def test_add_gold_qr_no_contract(open_crm_as_admin, lead_page, product_page):
     # Смена офиса
     lead_page.change_office_1_maya()
@@ -40,6 +44,7 @@ def test_add_gold_qr_no_contract(open_crm_as_admin, lead_page, product_page):
     product_page.take_screenshot()
 
 
+@pytest.mark.regress
 def test_add_silver_no_contract(open_crm_as_admin, lead_page, product_page):
     # Смена офиса
     lead_page.change_office_1_maya()
@@ -58,6 +63,7 @@ def test_add_silver_no_contract(open_crm_as_admin, lead_page, product_page):
     product_page.take_screenshot()
 
 
+@pytest.mark.extended
 def test_add_laptop_no_contract(open_crm_as_admin, lead_page, product_page):
     # Смена офиса
     lead_page.change_office_1_maya()
@@ -76,6 +82,7 @@ def test_add_laptop_no_contract(open_crm_as_admin, lead_page, product_page):
     product_page.take_screenshot()
 
 
+@pytest.mark.regress
 def test_add_car_no_contract(open_crm_as_admin, lead_page, product_page):
     # Смена офиса
     lead_page.change_office_krasnix_partisan()
